60958a7e03555d8c89e09b4b0b5c901ac94f52cb
임재혁(Harmon) <harmon@eipgrid.com>
2024-03-04 23:38:53 +0900

    refactor: SPA with history api

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
-import createRouter from "./router.js";
 import renderDesignPageTo from "./page/design-page.js";
 import renderDevPageTo from "./page/dev-page.js";
 
-const container = document.querySelector("main");
-const pages = {
-  renderDesignPage: () => renderDesignPageTo(container),
-  renderDevPage: () => renderDevPageTo(container),
-};
+document.querySelector("#design").addEventListener("click", () => {
+  history.pushState({}, "", "/#design");
+  renderDesignPageTo(document.querySelector("main"));
+});
 
-const router = createRouter();
-
-// 라우트 추가
-router.addRoute("#/design", pages.renderDesignPage);
-router.addRoute("#/dev", pages.renderDevPage);
-
-// 라우터 시작
-router.start();
-
-// add click event to logo, page to home
-document.querySelector(".logo-container").addEventListener("click", () => {
-  location.href = "/";
+document.querySelector("#dev").addEventListener("click", () => {
+  history.pushState({}, "", "/#dev");
+  renderDevPageTo(document.querySelector("main"));
 });
